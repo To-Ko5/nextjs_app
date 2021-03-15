9633631d0aec60c655d50b20d7f264fb659e38bb
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-15 22:51:40 +0900

    Add: footerを追加

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-  return <div></div>
+  return (
+    <footer className="w-full h-5 flex justify-center items-center text-gray-500 text-sm">
+      <p>next.js_app</p>
+    </footer>
+  )
 }
 
 export default Footer
