2fa75907df0f3632d7ac17f9352314941ed17e72
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-15 22:51:58 +0900

    Add: layoutを作成

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Head from 'next/head'
+import Footer from './common/Footer'
 
-const Layout = () => {
+interface Layout {
+  children: React.ReactChild
+  title?: string
+}
+
+const Layout = ({ children, title = 'next.js_app' }: Layout) => {
   return (
-    <div>
-      
+    <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      {children}
+      <Footer />
     </div>
   )
 }
