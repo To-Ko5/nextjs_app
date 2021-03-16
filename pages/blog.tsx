003a61ad383ee8bb2ab2d99f1a99fd9d561c072a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-16 23:02:44 +0900

    Add: linkを追加

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,5 +1,28 @@
+import Layout from '../components/Layout'
+import Link from 'next/link'
+
 const blog = () => {
-  return <div></div>
+  return (
+    <Layout title="blog">
+      <Link href="/main">
+        <svg
+          className="w-6 h-6 mr-3"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+          />
+        </svg>
+        <span>Back</span>
+      </Link>
+    </Layout>
+  )
 }
 
 export default blog
