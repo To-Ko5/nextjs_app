92f82d6426cc00ccb5a23cf0b986debe49e6b35e
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-18 08:01:14 +0900

    Add: functionを追加

--- lib/task.ts
+++ lib/task.ts
@@ -11,3 +11,39 @@ export async function getAllTaskData() {
 
   return response.data
 }
+
+export async function getAllPostIds() {
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`
+  )
+
+  if (response.status === 400) {
+    throw new Error('posts failed')
+  }
+
+  const res = await response.data
+
+  return res.map((task: any) => {
+    return {
+      params: {
+        id: String(task.id)
+      }
+    }
+  })
+}
+
+export async function getPostData(id: any) {
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/detail-task/${id}/`
+  )
+
+  if (response.status === 400) {
+    throw new Error('posts failed')
+  }
+
+  const task = response.data
+
+  return {
+    task
+  }
+}
