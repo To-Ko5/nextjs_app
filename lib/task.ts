421026ed8d09dba81857c4e242f2eea17435d367
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-18 08:14:42 +0900

    Fix: function名修正

--- lib/task.ts
+++ lib/task.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export async function getAllTaskData() {
+export async function getAllTasksData() {
   const response = await axios.get(
     `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`
   )
