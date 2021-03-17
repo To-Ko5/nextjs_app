286e2b8e69779fe404bedb80c980d2723d164a7b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-17 09:39:07 +0900

    Add: getAllPostsIds

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,5 +1,12 @@
 import axios from 'axios'
 
+interface post {
+  id: number
+  title: string
+  content: string
+  created_at: string
+}
+
 export async function getAllPostsData() {
   const response = await axios.get(
     'https://nextjs-rest-api1.herokuapp.com/api/list-post'
@@ -9,4 +16,19 @@ export async function getAllPostsData() {
     throw new Error('posts failed')
   }
   return response.data
+}
+
+export async function getAllPostsIds() {
+  const response = await axios.get(
+    'https://nextjs-rest-api1.herokuapp.com/api/list-post'
+  )
+  if (response.status === 400) {
+    throw new Error('posts failed')
+  }
+  const res = response.data
+  return res.map((post: post) => {
+    params: {
+      id: post.id
+    }
+  })
 }
