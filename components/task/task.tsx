430a0c123a429f34eec161f52bb4d79f05487336
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-18 08:36:25 +0900

    Fix: styleを修正

--- components/task/task.tsx
+++ components/task/task.tsx
@@ -2,6 +2,7 @@ const task = ({ task }: any) => {
   return (
     <div>
       <span>{task.id}</span>
+      <span>:</span>
       <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
         {task.title}
       </span>
