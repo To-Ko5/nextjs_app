5c248f81e601fcd5b79a512c563d8a584fedeaad
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-17 09:58:02 +0900

    Fix; styleを修正

--- components/blog/Post.tsx
+++ components/blog/Post.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link'
+
 const Post = ({ post }: any) => {
   return (
     <li>
-      <span>{post.id}</span>:
-      <span className="cursor-pointer border-b border-gray-400 hover:bg-gray-600">
-        {post.title}
-      </span>
+      <Link href={`/posts/${post.id}`}>
+        <a>
+          <span>{post.id}</span>:
+          <span className="cursor-pointer border-b border-gray-400 hover:bg-gray-600">
+            {post.title}
+          </span>
+        </a>
+      </Link>
     </li>
   )
 }
