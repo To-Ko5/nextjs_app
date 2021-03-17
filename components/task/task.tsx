2188b629267f1ac075a0236502de866839c69eb1
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-18 08:16:11 +0900

    Add: taskの表示を追加

--- components/task/task.tsx
+++ components/task/task.tsx
@@ -1,5 +1,12 @@
-const task = () => {
-  return <div></div>
+const task = ({ task }: any) => {
+  return (
+    <div>
+      <span>{task.id}</span>
+      <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
+        {task.title}
+      </span>
+    </div>
+  )
 }
 
 export default task
