1f31959a094662a49f5cc85c648f96443e591491
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-18 07:59:37 +0900

    Add: getAllTaskDataを追加

--- lib/task.ts
+++ lib/task.ts
@@ -1,1 +1,13 @@
 import axios from 'axios'
+
+export async function getAllTaskData() {
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`
+  )
+
+  if (response.status === 400) {
+    throw new Error('posts failed')
+  }
+
+  return response.data
+}
